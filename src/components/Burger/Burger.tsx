2c394dbf06a999e2185548906c5a76cd4da0bbb0
Chris Couzens <ccouzens@ukcloud.com>
2019-03-12 17:41:27 +0000

    Show message when there are no burger ingredients

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -3,15 +3,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const burger = (props: { ingredients: { [key: string]: number } }) => {
-  const transformedIngredients = (['salad', 'bacon', 'cheese', 'meat'] as (
+  let transformedIngredients = (['salad', 'bacon', 'cheese', 'meat'] as (
     | 'salad'
     | 'bacon'
     | 'cheese'
-    | 'meat')[]).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+    | 'meat')[])
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => [...arr, ...el], []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = [<p>Please Start Adding ingredients!</p>];
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
