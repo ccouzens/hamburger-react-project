6ee823dcfeb0c8978a3f2914fc3e7965abb85d13
Chris Couzens <ccouzens@ukcloud.com>
2019-03-12 17:30:40 +0000

    Read in ingredients from the Burger Builder class component
    It's a bit weird that we store as an Object and transform to an Array.
    It prevents chosing the order of the ingredients.

--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -3,10 +3,21 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state: {
+    ingredients: { salad: number; bacon: number; cheese: number; meat: number };
+  } = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </>
     );
