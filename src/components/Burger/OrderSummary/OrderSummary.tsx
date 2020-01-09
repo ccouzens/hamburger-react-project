232d2bd964d3b8c6eb96bf761ef4c2ac07746500
Chris Couzens <ccouzens@ukcloud.com>
2020-01-09 10:25:07 +0000

    Add price to order summary

--- src/components/Burger/OrderSummary/OrderSummary.tsx
+++ src/components/Burger/OrderSummary/OrderSummary.tsx
@@ -6,6 +6,7 @@ const orderSummary = (props: {
   ingredients: Map<IngredientType, number>;
   purchaseCancelled: () => void;
   purchaseContinued: () => void;
+  price: number;
 }) => {
   const ingredientSummary = [...props.ingredients].map(
     ([ingredient, amount]) => (
@@ -20,6 +21,9 @@ const orderSummary = (props: {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>
         CANCEL
