250ffc49a733be68e7257895ff7c556dcc08bf9c
Chris Couzens <ccouzens@ukcloud.com>
2020-01-13 16:35:07 +0000

    Replace menu with a hamburger icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.tsx
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import classes from './DrawerToggle.module.css';
 
 const drawerToggle = (props: { clicked: () => void }) => (
-  <div onClick={props.clicked}>Menu</div>
+  <div onClick={props.clicked} className={classes.DrawerToggle}>
+    <div />
+    <div />
+    <div />
+  </div>
 );
 
 export default drawerToggle;
