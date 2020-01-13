3f5a13c6d59e6fc9eac0385c4eb88d388b80508a
Chris Couzens <ccouzens@ukcloud.com>
2020-01-13 15:38:00 +0000

    Add sidedrawer for narrow devices

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React, { PropsWithChildren } from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props: PropsWithChildren<{}>) => (
   <>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </>
 );
