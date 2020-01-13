53a2d00a70405f84b2929a8dca22bff4659033ff
Chris Couzens <ccouzens@ukcloud.com>
2020-01-13 16:15:24 +0000

    Move the sidebar out when clicked off

--- src/components/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const sideDrawer = (props: {}) => {
+const sideDrawer = (props: { closed: () => void; open: boolean }) => {
+  let attachedClasses = `${classes.SideDrawer} ${classes.Close}`;
+  if (props.open) {
+    attachedClasses = `${classes.SideDrawer} ${classes.Open}`;
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </>
   );
 };
 
