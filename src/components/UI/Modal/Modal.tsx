8bb25c98e8f447ef90fef5b829cedf93427c91f4
Chris Couzens <ccouzens@ukcloud.com>
2020-01-22 13:50:35 +0000

    Only rerender the Modal on show and hide

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,23 +1,27 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useMemo } from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 const modal = (props: {
   children: ReactNode;
   show: boolean;
   modalClosed: () => void;
-}) => (
-  <>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? 1 : 0
-      }}
-    >
-      {props.children}
-    </div>
-  </>
-);
+}) =>
+  useMemo(
+    () => (
+      <>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: props.show ? 1 : 0
+          }}
+        >
+          {props.children}
+        </div>
+      </>
+    ),
+    [props.show]
+  );
 
 export default modal;
