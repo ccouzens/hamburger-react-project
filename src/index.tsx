8b159cc969208e16d33d55626e8be06dbeb03a54
Chris Couzens <ccouzens@gmail.com>
2020-02-01 19:05:34 +0000

    Add axios interceptors
    I'm not a fan of configuring something on a global level.

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
 
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    return request;
+  },
+  error => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  response => {
+    console.log(response);
+    return response;
+  },
+  error => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
 ReactDOM.render(<App />, document.getElementById("root"));
