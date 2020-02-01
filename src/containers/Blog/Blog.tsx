19d5f74bfb97cbaafcff26697c69b8de6e5ebb23
Chris Couzens <ccouzens@gmail.com>
2020-02-01 17:19:29 +0000

    Retrieve posts from the web

--- src/containers/Blog/Blog.tsx
+++ src/containers/Blog/Blog.tsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
-const Blog = (props: {}) => (
-  <div>
-    <section className="Posts">
-      <Post />
-      <Post />
-      <Post />
-    </section>
-    <section>
-      <FullPost />
-    </section>
-    <section>
-      <NewPost />
-    </section>
-  </div>
-);
+interface Post {
+  id: number;
+  title: string;
+  author: string;
+}
+
+const Blog = (props: {}) => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const canceller = axios.CancelToken.source();
+    axios
+      .get<{ id: number; title: string }[]>(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          cancelToken: canceller.token
+        }
+      )
+      .then(r => {
+        const posts = r.data
+          .slice(0, 4)
+          .map(post => ({ ...post, author: "Max" }));
+        setPosts(posts);
+      });
+    return function cleanup() {
+      canceller.cancel();
+    };
+  });
+  return (
+    <div>
+      <section className="Posts">
+        {posts.map(post => (
+          <Post key={post.id} title={post.title} author={post.author} />
+        ))}
+      </section>
+      <section>
+        <FullPost />
+      </section>
+      <section>
+        <NewPost />
+      </section>
+    </div>
+  );
+};
 
 export default Blog;
